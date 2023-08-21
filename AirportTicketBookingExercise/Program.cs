44ba76fb3002dc4add7f78f2557894649979fbf2
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-08-21 09:47:59 +0300

    First commit

---
+++ AirportTicketBookingExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AirportTicketBookingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
